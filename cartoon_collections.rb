ca24cea80b150dce97bfd17b62b4b3025ad04ad3
Jerry Lee <jwl62@georgetown.edu>
2018-02-28 22:13:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,11 +13,15 @@ def summon_captain_planet(arr)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arr)# code an argument here
   # Your code here
+  arr.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  (arr&cheese_types)[0]
 end
